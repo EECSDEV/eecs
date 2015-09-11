ddaa2b5d252bd0a485c7509a804464c9b9d4fb8f
ma405th <ma405th@gmail.com>
2015-09-11 02:53:12 -0700

    bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911074051) do
+ActiveRecord::Schema.define(version: 20150911085613) do
 
   create_table "adminparams", force: :cascade do |t|
     t.integer  "point_get_feedback"
